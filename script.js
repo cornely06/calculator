8ccbda7e1c216310f0652f1c1058c873ab920083
Raymond Cornely <cornely06@gmail.com>
2021-01-27 05:55:54 -0500

    Add digits to calculator

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ function operate(operator, a, b) {
     return operator(a, b);
 }
 console.log(operate(add, 5, 2));
+
+let numPad = function(){
+    let digits = document.querySelector("#digits");
+    let i = 1;
+    while(i < 10) {
+        let newNum = document.createElement("button");
+        newNum.textContent = i;
+        newNum.classList.add("num");
+        digits.appendChild(newNum);
+        i++;
+    }
+}
+numPad();
