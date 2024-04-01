f947cc59c0e734019c1e8a46fd5765fa8156236c
rc <59358349+cornely06@users.noreply.github.com>
2024-04-01 01:31:38 -0400

    Add operate function

--- js/script.js
+++ js/script.js
@@ -13,3 +13,24 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(firstNumber, secondNumber, operator) {
+  switch (operator) {
+    case "+":
+      console.log(add(firstNumber, secondNumber));
+      break;
+    case "-":
+      console.log(subtract(firstNumber, secondNumber));
+      break;
+    case "*":
+      console.log(multiply(firstNumber, secondNumber));
+      break;
+    case "/":
+      console.log(divide(firstNumber, secondNumber));
+      break;
+  }
+}
